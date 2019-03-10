5281ee77fb8d56c0950e81740eefcd6353aa66a4
Chris Stranex <chris@stranex.com>
2019-03-10 19:35:44 +0200

    Add drag and drop support for proxy users and groups

--- idefix-config.py
+++ idefix-config.py
@@ -355,11 +355,15 @@ class Idefix:
             self.arw[textView].connect("drag-data-received", self.on_drag_data_received)
 
         self.arw["proxy_users"].enable_model_drag_source(Gdk.ModifierType.BUTTON1_MASK, [], DRAG_ACTION)
+        self.arw['proxy_users'].drag_source_add_text_targets()
+        self.arw['proxy_users'].connect("drag-data-get", self.proxy_users_data_get)
         self.arw['proxy_users'].drag_dest_set(Gtk.DestDefaults.DROP, [], DRAG_ACTION)
         self.arw['proxy_users'].drag_dest_add_text_targets()
         self.arw['proxy_users'].connect("drag-data-received", self.update_proxy_user_list_view)
 
         self.arw["proxy_group"].enable_model_drag_source(Gdk.ModifierType.BUTTON1_MASK, [], DRAG_ACTION)
+        self.arw['proxy_group'].drag_source_add_text_targets()
+        self.arw['proxy_group'].connect("drag-data-get", self.proxy_group_data_get)
         self.arw['proxy_group'].drag_dest_set(Gtk.DestDefaults.DROP, [], DRAG_ACTION)
         self.arw['proxy_group'].drag_dest_add_text_targets()
         self.arw['proxy_group'].connect("drag-data-received", self.update_proxy_group_list_view)
@@ -1356,6 +1360,43 @@ class Idefix:
         if self.proxy_user_has_any():
             return
 
+        position = None
+
+        if time.time() - self.mem_time < 1:  # dirty workaround to prevent two drags
+            return
+        self.mem_time = time.time()
+
+        model = widget.get_model()
+
+        path = data.get_text()
+        try:
+            iter_source = model.get_iter(path)
+            values = [model.get_value(iter_source, i) for i in range(model.get_n_columns())]
+        except TypeError:
+            iter_source = None
+            values = None
+
+        dest = widget.get_dest_row_at_pos(x, y)
+        if dest:
+            drop_path, position = dest
+            iter1 = model.get_iter(drop_path)
+
+            if (position == Gtk.TreeViewDropPosition.BEFORE
+                    or position == Gtk.TreeViewDropPosition.INTO_OR_BEFORE):
+                iter_dest = model.insert_before(iter1, ['' for x in range(model.get_n_columns())])
+            else:
+                iter_dest = model.insert_after(iter1, ['' for x in range(model.get_n_columns())])
+        else:
+            iter_dest = model.insert(-1)
+
+        if iter_source:
+            for i in range(model.get_n_columns()):
+                model.set_value(iter_dest, i, model.get_value(iter_source, i))
+            model.remove(iter_source)
+            names = [name[0] for name in model]
+            self.proxy_store.set_value(self.iter_proxy, 5, '\n'.join(names))
+            return
+
         names = self.proxy_store.get_value(self.iter_proxy, 5).split('\n')
         if new_name in names:
             return
@@ -1365,6 +1406,44 @@ class Idefix:
 
     def update_proxy_group_list_view(self, widget, ctx, x, y, data, info, etime):
         """Add a proxy group to the list"""
+
+        position = None
+
+        if time.time() - self.mem_time < 1:  # dirty workaround to prevent two drags
+            return
+        self.mem_time = time.time()
+
+        model = widget.get_model()
+
+        path = data.get_text()
+        try:
+            iter_source = model.get_iter(path)
+            values = [model.get_value(iter_source, i) for i in range(model.get_n_columns())]
+        except TypeError:
+            iter_source = None
+            values = None
+
+        dest = widget.get_dest_row_at_pos(x, y)
+        if dest:
+            drop_path, position = dest
+            iter1 = model.get_iter(drop_path)
+
+            if (position == Gtk.TreeViewDropPosition.BEFORE
+                    or position == Gtk.TreeViewDropPosition.INTO_OR_BEFORE):
+                iter_dest = model.insert_before(iter1, ['' for x in range(model.get_n_columns())])
+            else:
+                iter_dest = model.insert_after(iter1, ['' for x in range(model.get_n_columns())])
+        else:
+            iter_dest = model.insert(-1)
+
+        if iter_source:
+            for i in range(model.get_n_columns()):
+                model.set_value(iter_dest, i, values[i])
+            model.remove(iter_source)
+            names = [name[0] for name in model]
+            self.proxy_store.set_value(self.iter_proxy, 7, '\n'.join(names))
+            return
+
         new_name = data.get_text().strip()
 
         names = self.proxy_store.get_value(self.iter_proxy, 7).split('\n')
@@ -1983,6 +2062,21 @@ class Idefix:
         if iter1:
             path = model.get_string_from_iter(iter1)
             data.set_text(path, -1)
+
+    def proxy_users_data_get(self, treeview, drag_context, data, info, time):
+
+        (model, iter1) = treeview.get_selection().get_selected()
+        if iter1:
+            path = model.get_string_from_iter(iter1)
+            data.set_text(path, -1)
+
+    def proxy_group_data_get(self, treeview, drag_context, data, info, time):
+
+        (model, iter1) = treeview.get_selection().get_selected()
+        if iter1:
+            path = model.get_string_from_iter(iter1)
+            data.set_text(path, -1)
+            print("DRAG", path)
 
     def users_drag_data_received(self, treeview, drag_context, x, y, data, info, etime):
 
