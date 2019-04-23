18095c3daf44ba751b781e190a10e091d9d37268
Averell7 <dy1977@orange.fr>
2019-04-23 20:43:09 +0200

    assisant update

--- assistant.py
+++ assistant.py
@@ -3,7 +3,8 @@ import time
 from gi.repository import Gdk, Gtk
 
 from myconfigparser import myConfigParser,myTextParser
-from util import askyesno, ask_text, mac_address_test
+from util import askyesno, ask_text, mac_address_test, showwarning
+import re
 
 
 class Assistant:
@@ -24,6 +25,23 @@ class Assistant:
             label1 = label + ".fr"
             if label1 in self.longtexts:
                 self.arw2[label].set_text(self.longtexts[label1])
+
+
+        # Listview
+        self.categories_store = Gtk.ListStore(str,Gtk.TreeIter,str,int)
+        self.arw2["assistant_categories"].set_model(self.categories_store)
+
+        self.cell = Gtk.CellRendererText()
+        self.radio = Gtk.CellRendererToggle(activatable=True, radio = True, xalign=0.5)
+        self.radio.connect('toggled', self.categories_radio_toggle)
+
+        self.tvcolumn = Gtk.TreeViewColumn(_('Key'), self.cell, text=0, foreground=15, background=16)
+        self.tvcolumn.set_fixed_width(220)
+        self.arw2["assistant_categories"].append_column(self.tvcolumn)
+        self.tvcolumn = Gtk.TreeViewColumn(_('---'), self.radio, active=3)
+        self.arw2["assistant_categories"].append_column(self.tvcolumn)
+
+
         # Listview
         self.arw2["assistant_proxy_rules"].set_model(self.controller.proxy_users.proxy_store)
 
@@ -78,94 +96,33 @@ class Assistant:
             return
         if self.arw2["check_filter"].get_active() == 1:
             self.arw2["check_full"].set_active(False)
-            self.arw2["check_full"].set_sensitive(False)
-        else:
-            self.arw2["check_full"].set_sensitive(True)
 
     def assistant_check_full(self, widget):
         if self.block_signals:
             return
         if self.arw2["check_full"].get_active() == 1:
             self.arw2["check_filter"].set_active(False)
-            self.arw2["check_filter"].set_sensitive(False)
-        else:
-            self.arw2["check_filter"].set_sensitive(True)
+            self.arw2["check_email"].set_active(True)
 
 
-
-
-    def add_address(self, widget = None):
-        return
 
     def ass_new_user(self, widget, a = None):
         self.arw2["assistant1"].set_page_complete(self.arw2["new_user"], True)
 
     def ass_firewall_permissions(self, widget, event = None):
         self.arw2["assistant1"].set_page_complete(self.arw2["firewall_permissions"], True)
+        self.update_categories_list()
 
-    def ass_firewalll_permissions(self, widget, event = None):
-        pass
-
-    def ass_new_user_rules(self, widget, event = None):
-        self.arw2["assistant1"].set_page_complete(self.arw2["proxy_rules"], True)
-
-    def get_mac_address(self):
-        mac = []
-        for index in["A", "B", "C", "D", "E", "F"]:
-            mac.append(self.arw2["mac_" + index].get_text())
-        mac = ":".join(mac)
-        return mac
-
-    def check_mac_address(self, widget, a = None):
-        mac = self.get_mac_address()
-        print(mac)
-        x = mac_address_test(mac)
-        print(x)
-
-
-    def choose_rules(self, widget, row):
-
-        self.controller.proxy_store[row][19] = not self.controller.proxy_store[row][19]
-##        if self.controller.proxy_store[row][19] == 0:
-##            self.controller.proxy_store[row][19] = 1
-##        else:
-##            self.controller.proxy_store[row][19] = 0
-
-
-
-
-
-    def summary(self, widget):
-        username = self.arw2["new_user_entry"].get_text()
-        mac_address = self.get_mac_address()
-        # firewall permissions
-
-        if self.arw2["radio_nothing"].get_active() == 1:
-            email = 0
-            web = 0
-            webfilter = 0
-            webfull = 0
-        elif self.arw2["radio_email"].get_active() == 1:
-            email = 1
-            web = 0
-            webfilter = 0
-            webfull = 0
-        elif self.arw2["radio_webonly"].get_active() == 1:
-            email = 0
-            web = 1
-            webfilter = 1
-            webfull = 0
-        elif self.arw2["radio_filter"].get_active() == 1:
-            email = 1
-            web = 1
-            webfilter = 1
-            webfull = 0
-        elif self.arw2["radio_full"].get_active() == 1:
-            email = 1
-            web = 1
-            webfilter = 0
-            webfull = 1
-
+    def update_categories_list(self,widget = None):
+        """ updates the list of categories which correspond to the choices for email, web etc. """
+        self.categories_store.clear()
+        email = self.arw2["check_email"].get_active()
+        webfilter = self.arw2["check_filter"].get_active()
+        webfull = self.arw2["check_full"].get_active()
+        if webfilter or webfull:
+            web = True
+        else:
+            web = False
 
         # get category
         for row in self.controller.users_store:
@@ -174,7 +131,66 @@ class Assistant:
                 and row[6] == webfilter
                 and row[7] == webfull):
                 iter1 = row.iter
-                break
+                path1 = row.path.to_string()
+                name = row[0]
+                iternew = self.categories_store.append()
+                self.categories_store.set_value(iternew,0,name)
+                self.categories_store.set_value(iternew,2,path1)
+                self.categories_store.set_value(iternew,3,0)
+        try:
+            self.categories_store[0][3] = 1
+        except:
+            pass
+
+    def categories_radio_toggle(self, widget, row):
+        """ controls the toggle buttons column """
+        for row1 in self.categories_store:
+            row1[3] = 0
+        self.categories_store[row][3] = 1
+
+    def ass_new_user_rules(self, widget, event = None):
+        self.arw2["assistant1"].set_page_complete(self.arw2["proxy_rules"], True)
+
+    def get_mac_address(self):
+        mac = []
+        for index in["A", "B", "C", "D", "E", "F"]:
+            value = self.arw2["mac_" + index].get_text().lower()
+            x = re.search("[0-9a-f]{2}", value)
+            if not x:
+                showwarning(_("Error"), _("Invalid Mac address, please correct and retry"))
+                return False
+
+            mac.append(value)
+        mac = ":".join(mac)
+        return mac
+
+    def reset_mac_address(self):
+        for index in["A", "B", "C", "D", "E", "F"]:
+            self.arw2["mac_" + index].set_text("")
+
+
+    def check_mac_address(self, widget, a = None):
+        mac = self.get_mac_address()
+        print(mac)
+        x = mac_address_test(mac)
+        print(x)
+
+    def add_address(self, widget = None):
+        address = self.get_mac_address()
+        if address:
+            self.reset_mac_address()
+            self.arw2["new_user_mac"].get_buffer().insert_at_cursor(address + "\n")
+
+
+    def choose_rules(self, widget, row):
+        """ controls the toggle buttons column """
+        self.controller.proxy_store[row][19] = not self.controller.proxy_store[row][19]
+
+    def summary(self, widget):
+        username = self.arw2["new_user_entry"].get_text()
+        mac_address = self.get_mac_address()
+
+
 
         # TODO : create category if needed
         """
@@ -193,10 +209,27 @@ class Assistant:
         12 : icon 2
         """
         # create user
+        # get the selected category
+        for row1 in self.categories_store:
+            if row1[3] == 1:
+                category = row1[0]
+                print(category)
+                path1 = row1[2]
+                print(path1)
+                break
+        iter1 = self.controller.users_store.get_iter_from_string(path1)
         iternew = self.controller.users_store.insert(iter1, 1,
-                        [username, "", "", "", email, web, webfilter, webfull, 0, "", "", None, None])
+                        [username, "", "", "", 0, 0, 0, 0, 0, "", "", None, None])
         self.controller.maclist[username] = [mac_address]
         self.controller.set_colors()
+
+        if self.arw2["check_filter"].get_active() == 0:
+            sel = self.controller.arw["treeview1"].get_selection()
+            sel.select_iter(iter1)
+            self.controller.arw["notebook3"].set_current_page(0)
+            self.arw2["assistant1"].hide()
+            return
+
 
         # Proxy config
         if self.arw2["radio_specific_rule"].get_active() == 1:
