5535d8bcb7652ca004da24bdc6c58e4262b44c66
Averell7 <dy1977@orange.fr>
2019-06-06 21:08:07 +0200

    Update repository.py

--- repository.py
+++ repository.py
@@ -1,24 +1,20 @@
-import json
-from urllib.error import HTTPError
-from urllib.request import urlopen
+import requests
 
 REPOSITORY_URL = 'http://proxy-groups.idefix64.fr/proxy-groups2.json'
 
 
 def fetch_repository_list(url=REPOSITORY_URL):
     """Fetch the repository listing from the given url"""
-    try:
-        with urlopen(url) as request:
-            if request.status != 200:
-                return None
-
-            return json.load(request)
-    except HTTPError:
+    result = requests.get(url)
+    if result.ok:
+        return result.json()
+    else:
         return None
 
 
 def download_group_file(path):
     """Download the group ini file"""
+<<<<<<< HEAD
     try:
         with urlopen(path) as request:
             if request.status != 200:
@@ -27,3 +23,7 @@ def download_group_file(path):
             return request.read()
     except HTTPError:
         return None
+=======
+    result = requests.get(path)
+    return result.content
+>>>>>>> parent of 33023a4... Remove requests in favour of urllib
