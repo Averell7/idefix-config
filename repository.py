29de37435e414203c6733ba555135ca59796dbb7
Chris Stranex <chris@stranex.com>
2019-06-06 21:18:57 +0200

    Revert merge

--- repository.py
+++ repository.py
@@ -14,16 +14,5 @@ def fetch_repository_list(url=REPOSITORY_URL):
 
 def download_group_file(path):
     """Download the group ini file"""
-<<<<<<< HEAD
-    try:
-        with urlopen(path) as request:
-            if request.status != 200:
-                return None
-
-            return request.read()
-    except HTTPError:
-        return None
-=======
     result = requests.get(path)
     return result.content
->>>>>>> parent of 33023a4... Remove requests in favour of urllib
