0f823ed3512f4db94565415cb87cfa4e6ae3dc70
Chris Stranex <cstranex@gmail.com>
2019-10-26 18:53:21 +0200

    Add basic start to group admin tool

---
+++ groupdbmanager/db.py
@@ -0,0 +1,99 @@
+"""
+Connect to the remote database to do operations
+"""
+import json
+import mysql.connector
+
+
+class Database:
+    def __init__(self, host, port, username, password, database):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.database = database
+        self.db = None
+
+    def connect(self):
+        """Connect to the db"""
+        self.db = mysql.connector.connect(
+            host=self.host,
+            user=self.username,
+            passwd=self.password,
+            port=self.port,
+            db=self.database,
+            auth_plugin='mysql_native_password'
+        )
+        self.db.autocommit = True
+
+    def disconnect(self):
+        """Disconnect from the db"""
+        self.db.close()
+        self.db = None
+
+    @property
+    def connected(self):
+        return self.db is not None
+
+    def get_groups(self, verified=None, name=None, domains=None, category=None):
+        """Return a list of groups"""
+        rows = self.db.cursor()
+        rows.execute("SELECT id, name, domains, unverified, category_id FROM proxy_groups")
+        for id, name, domains, unverified, category_id in rows:
+
+            # Convert domain list from json list to new-line separated string
+            if domains:
+                domains = '\n'.join([domain for domain in json.loads(domains)])
+            else:
+                domains = ''
+
+            yield {
+                'id': id,
+                'name': name,
+                'domains': domains,
+                'unverified': unverified,
+                'category_id': category_id
+            }
+        rows.close()
+
+    def get_categories(self, category=None):
+        """Return a list of categories"""
+        rows = self.db.cursor()
+        rows.execute("SELECT id, name, parent_id FROM proxy_category ORDER BY parent_id")
+        for id, name, parent_id in rows:
+            yield {
+                'id': id,
+                'name': name,
+                'parent_id': parent_id,
+            }
+        rows.close()
+
+    def update_group(self, group_id, verified, name, domains):
+        """Update the group with the given details"""
+
+        cur = self.db.cursor()
+        cur.execute("UPDATE proxy_groups SET unverified=%s, name=%s, domains=%s WHERE id=%s LIMIT 1", (
+            not verified,
+            name,
+            json.dumps(domains),
+            group_id
+        ))
+        cur.close()
+
+    def update_category(self, category, name, parent_id):
+        """Update the category with the given details"""
+        pass
+
+    def delete_group(self, group_id):
+        """Remove the group by its id"""
+        pass
+
+    def delete_category(self, category_id):
+        """Delete the category by its id"""
+        pass
+
+    def create_group(self, verified, name, domains, category_id):
+        """Create a new group and return its id"""
+
+    def create_category(self, name, parent_id):
+        """Create a new category and return its id"""
---
+++ groupdbmanager/main.py
@@ -0,0 +1,203 @@
+from gi.repository import Gtk
+from db import Database
+import configparser
+
+
+COLUMN_NAME = 0
+COLUMN_DATA = 1
+COLUMN_ID = 2
+COLUMN_TYPE = 3
+COLUMN_VERIFIED = 4
+TYPE_CATEGORY = 0
+TYPE_GROUP = 1
+
+
+class DatabaseManager:
+
+    category_iters = {}
+
+    def __init__(self):
+        widgets = Gtk.Builder()
+        widgets.set_translation_domain("confix")
+        widgets.add_from_file('./main.glade')
+        widgets.connect_signals(self)
+        self.widgets = {}
+        for z in widgets.get_objects():
+            try:
+                name = Gtk.Buildable.get_name(z)
+                self.widgets[name] = z
+                z.name = name
+            except:
+                pass
+        self.widgets['main_window'].show()
+
+        self.config = configparser.ConfigParser(interpolation=None)
+        self.config.read('config.ini')
+
+        self.store = self.widgets['group_store']
+        self.widgets['verified_treeview'].get_model().set_visible_func(self.filter_verified)
+        self.widgets['unverified_treeview'].get_model().set_visible_func(self.filter_unverified)
+
+        self.database = Database(
+            self.config['database']['host'],
+            self.config.getint('database', 'port', fallback=3306),
+            self.config['database']['username'],
+            self.config['database']['password'],
+            self.config['database']['database'],
+        )
+
+    def filter_verified(self, model, iter, data=None):
+        """Return true if the iter is verified. Also hides categories that don't have any groups"""
+        if model.get_value(iter, COLUMN_TYPE) == TYPE_GROUP:
+            return model.get_value(iter, COLUMN_VERIFIED)
+        else:
+            child = model.iter_children(iter)
+            while child:
+                if self.filter_verified(model, child):
+                    return True
+                child = model.iter_next(child)
+            return False
+
+    def filter_unverified(self, model, iter, data=None):
+        """Return true if the iter is verified"""
+        if model.get_value(iter, COLUMN_TYPE) == TYPE_GROUP:
+            return not model.get_value(iter, COLUMN_VERIFIED)
+        else:
+            child = model.iter_children(iter)
+            while child:
+                if self.filter_unverified(model, child):
+                    return True
+                child = model.iter_next(child)
+            return False
+
+    def verified_selected(self, widget):
+        model, iter = widget.get_selected()
+        if not iter:
+            return
+        iter = model.convert_iter_to_child_iter(iter)
+        model = model.get_model()
+
+        if model.get_value(iter, COLUMN_TYPE) == TYPE_GROUP:
+            self.widgets['verified_entry_name'].set_text(model.get_value(iter, COLUMN_NAME))
+            self.widgets['verified_entry_checkbox'].set_active(model.get_value(iter, COLUMN_VERIFIED))
+            buffer = Gtk.TextBuffer()
+            buffer.set_text(model.get_value(iter, COLUMN_DATA))
+            self.widgets['verified_textview'].set_buffer(buffer)
+        else:
+            self.widgets['verified_entry_name'].set_text('')
+            self.widgets['verified_entry_checkbox'].set_active(False)
+            self.widgets['verified_textview'].get_buffer().set_text('')
+
+    def unverified_selected(self, widget):
+        model, iter = widget.get_selected()
+        if not iter:
+            return
+        iter = model.convert_iter_to_child_iter(iter)
+        model = model.get_model()
+
+        if model.get_value(iter, COLUMN_TYPE) == TYPE_GROUP:
+            self.widgets['unverified_entry_name'].set_text(model.get_value(iter, COLUMN_NAME))
+            self.widgets['unverified_entry_checkbox'].set_active(model.get_value(iter, COLUMN_VERIFIED))
+            buffer = Gtk.TextBuffer()
+            buffer.set_text(model.get_value(iter, COLUMN_DATA))
+            self.widgets['unverified_textview'].set_buffer(buffer)
+        else:
+            self.widgets['unverified_entry_name'].set_text('')
+            self.widgets['unverified_entry_checkbox'].set_active(False)
+            self.widgets['unverified_textview'].get_buffer().set_text('')
+
+    def updated_unverified(self, widget):
+        """Update the selected group"""
+        model, iter = self.widgets['unverified_treeview'].get_selection().get_selected()
+        iter = model.convert_iter_to_child_iter(iter)
+        model = model.get_model()
+
+        # Get the id
+        id = model.get_value(iter, COLUMN_ID)
+
+        # Update with the entry data
+        buffer = self.widgets['unverified_textview'].get_buffer()
+        data = buffer.get_text(
+            buffer.get_start_iter(),
+            buffer.get_end_iter(),
+            False
+        )
+        name = self.widgets['unverified_entry_name'].get_text()
+        verified = self.widgets['unverified_entry_checkbox'].get_active()
+
+        self.database.update_group(
+            int(id),
+            verified,
+            name,
+            data.split('\n')
+        )
+        self.refresh_database()
+
+    def updated_verified(self, widget):
+        """Update the selected group"""
+        model, iter = self.widgets['verified_treeview'].get_selection().get_selected()
+        iter = model.convert_iter_to_child_iter(iter)
+        model = model.get_model()
+
+        # Get the id
+        id = model.get_value(iter, COLUMN_ID)
+
+        # Update with the entry data
+        buffer = self.widgets['verified_textview'].get_buffer()
+        data = buffer.get_text(
+            buffer.get_start_iter(),
+            buffer.get_end_iter(),
+            False
+        )
+        name = self.widgets['verified_entry_name'].get_text()
+        verified = self.widgets['verified_entry_checkbox'].get_active()
+
+        self.database.update_group(
+            int(id),
+            verified,
+            name,
+            data.split('\n')
+        )
+        self.refresh_database()
+
+    def connect_database(self, widget):
+        if not self.database.connected:
+            self.database.connect()
+            self.refresh_database()
+        else:
+            self.database.disconnect()
+
+    def refresh_database(self, widget=None):
+        """Refresh the tree views"""
+        self.store.clear()
+        self.category_iters = {}
+
+        for row in self.database.get_categories():
+
+            iter = self.store.append(self.category_iters.get(row['id'], None))
+
+            self.category_iters[row['id']] = iter
+            self.store.set_value(iter, COLUMN_ID, str(row['id']))
+            self.store.set_value(iter, COLUMN_VERIFIED, True)
+            self.store.set_value(iter, COLUMN_TYPE, TYPE_CATEGORY)
+            self.store.set_value(iter, COLUMN_NAME, row['name'])
+
+        for row in self.database.get_groups():
+            parent_iter = self.category_iters.get(row['category_id'])
+
+            iter = self.store.append(parent_iter)
+            self.store.set_value(iter, COLUMN_ID, str(row['id']))
+            self.store.set_value(iter, COLUMN_VERIFIED, not row['unverified'])
+            self.store.set_value(iter, COLUMN_NAME, row['name'])
+            self.store.set_value(iter, COLUMN_TYPE, TYPE_GROUP)
+            self.store.set_value(iter, COLUMN_DATA, row['domains'])
+
+    def quit(self, *args):
+        if self.database.connected:
+            self.database.disconnect()
+        Gtk.main_quit()
+
+
+if __name__ == '__main__':
+    manager = DatabaseManager()
+    Gtk.main()
