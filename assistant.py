7a6a2a2dc3f8c611532f068f64c82aff57a5be55
Chris Stranex <chris@stranex.com>
2020-02-17 20:08:01 +0200

    Fix password encryption/decryption

--- assistant.py
+++ assistant.py
@@ -497,8 +497,8 @@ class Assistant:
     def finish_first_configuration(self):
         """Open connection profile and hide the assistant"""
         self.controller.update_gui()
-        self.controller.list_configuration_profiles()
-        self.controller.profiles.profile_save_config()  # ensures the passwords are stored encrypted
+        self.controller.profiles.refresh_saved_profiles()
+        self.controller.profiles.profile_save_config()
         self.arw["configname"].set_text("default")
         self.controller.open_connexion_profile()
         self.arw2['first_use_assistant'].hide()
